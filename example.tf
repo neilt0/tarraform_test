bd2eebce1536b75d738f8c2fcfd22984573286fb
Neil Travers <neiltravers+github@gmail.com>
2018-09-06 17:58:57 +0100

    Add input variables

--- example.tf
+++ example.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region     = "eu-west-2"
+  region     = "${var.region}"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-78e8fd1c"
+  ami           = "${lookup(var.amis, var.region)}"
   instance_type = "t2.micro"
   
   provisioner "local-exec" {
@@ -11,5 +11,17 @@ resource "aws_instance" "example" {
   }  
 }
 
+variable "amis" {
+  type = "map"
+  default = {
+    "us-east-1" = "ami-b374d5a5"
+    "us-west-2" = "ami-4b32be2b"
+	"eu-west-2" = "ami-78e8fd1c"
+  }
+}
+
+variable "region" {
+  default = "eu-west-2"
+}
 
 # London eu-west-2  ami-ed908589 ami-78e8fd1c
