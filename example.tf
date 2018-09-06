bcbf5151119f48d8b23a445ed6823a0ac0319b48
Neil Travers <neiltravers+github@gmail.com>
2018-09-06 15:28:13 +0100

    Add EIP as an example

--- example.tf
+++ example.tf
@@ -2,6 +2,10 @@ provider "aws" {
   region     = "eu-west-2"
 }
 
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-78e8fd1c"
   instance_type = "t2.micro"
