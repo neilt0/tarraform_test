c5c2a5d33135b3992106d1fc48dea602bf70d176
Neil Travers <neiltravers+github@gmail.com>
2018-09-06 16:08:01 +0100

    Adds provisioner

--- example.tf
+++ example.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region     = "eu-west-2"
 }
 
-resource "aws_eip" "ip" {
-  instance = "${aws_instance.example.id}"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-78e8fd1c"
   instance_type = "t2.micro"
+  
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }  
 }
 
 
