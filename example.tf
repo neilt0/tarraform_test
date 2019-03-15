a9e40b6dbd930b0aeb3b86e970d53561ac426484
Neil Travers <neilt@localhost>
2019-03-15 13:09:59 +0000

    change AMI to Ubuntu 18.04

--- example.tf
+++ example.tf
@@ -16,7 +16,7 @@ variable "amis" {
   default = {
     "us-east-1" = "ami-b374d5a5"
     "us-west-2" = "ami-4b32be2b"
-	"eu-west-2" = "ami-78e8fd1c"
+	"eu-west-2" = "ami-07dc734dc14746eab"
   }
 }
 
@@ -24,4 +24,4 @@ variable "region" {
   default = "eu-west-2"
 }
 
-# London eu-west-2  ami-ed908589 ami-78e8fd1c
+# London eu-west-2  ami-ed908589 ami-78e8fd1c  ami-07dc734dc14746eab
